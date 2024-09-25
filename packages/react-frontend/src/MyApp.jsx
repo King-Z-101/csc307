1a49ab442831a018d093b16b061db85d9a7a486e
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2024-09-25 11:42:11 -0700

    After Step 2

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,13 @@
+// src/MyApp.jsx
+import React from "react";
+import Table from "./Table";
+
+function MyApp() {
+  return (
+    <div className="container">
+      <Table />
+    </div>
+  );
+}
+
+export default MyApp;
