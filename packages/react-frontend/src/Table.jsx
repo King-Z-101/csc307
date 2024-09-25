d69060dea4b41615d9bf6a88ca331f37f2dc12b3
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2024-09-25 11:56:55 -0700

    After Step 3

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -1,33 +1,47 @@
 // src/Table.jsx
+// src/Table.jsx
 import React from "react";
+
+
+function TableHeader() {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  );
+}
+
+function TableBody() {
+  return (
+    <tbody>
+      <tr>
+        <td>Charlie</td>
+        <td>Janitor</td>
+      </tr>
+      <tr>
+        <td>Mac</td>
+        <td>Bouncer</td>
+      </tr>
+      <tr>
+        <td>Dee</td>
+        <td>Aspiring actress</td>
+      </tr>
+      <tr>
+        <td>Dennis</td>
+        <td>Bartender</td>
+      </tr>
+    </tbody>
+  );
+}
 
 function Table() {
   return (
     <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
-        </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Bouncer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-          <td>Bartender</td>
-        </tr>
-      </tbody>
+      <TableHeader />
+      <TableBody />
     </table>
   );
 }
