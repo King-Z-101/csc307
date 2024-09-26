b069af6e80e906bcbfeee9672272dbb5258df505
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2024-09-25 22:59:25 -0700

    Next: Step 5

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -2,10 +2,29 @@
 import React from "react";
 import Table from "./Table";
 
+const characters = [
+  {
+    name: "Charlie",
+    job: "Janitor"
+  },
+  {
+    name: "Mac",
+    job: "Bouncer"
+  },
+  {
+    name: "Dee",
+    job: "Aspring actress"
+  },
+  {
+    name: "Dennis",
+    job: "Bartender"
+  }
+];
+
 function MyApp() {
   return (
     <div className="container">
-      <Table />
+      <Table characterData={characters} />
     </div>
   );
 }
--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -14,34 +14,28 @@ function TableHeader() {
   );
 }
 
-function TableBody() {
-  return (
-    <tbody>
-      <tr>
-        <td>Charlie</td>
-        <td>Janitor</td>
+function TableBody(props) {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
       </tr>
-      <tr>
-        <td>Mac</td>
-        <td>Bouncer</td>
-      </tr>
-      <tr>
-        <td>Dee</td>
-        <td>Aspiring actress</td>
-      </tr>
-      <tr>
-        <td>Dennis</td>
-        <td>Bartender</td>
-      </tr>
-    </tbody>
+    );
+   }
   );
+  return (
+      <tbody>
+        {rows}
+       </tbody>
+   );
 }
 
-function Table() {
+function Table(props) {
   return (
     <table>
       <TableHeader />
-      <TableBody />
+      <TableBody characterData={props.characterData} />
     </table>
   );
 }
