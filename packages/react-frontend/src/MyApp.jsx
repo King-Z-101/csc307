d1eb26134172ef903a0a1e54895962b737c6afa3
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2024-09-27 11:49:38 -0700

    Step 6 done

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,32 +1,62 @@
 // src/MyApp.jsx
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
 
-const characters = [
-  {
-    name: "Charlie",
-    job: "Janitor"
-  },
-  {
-    name: "Mac",
-    job: "Bouncer"
-  },
-  {
-    name: "Dee",
-    job: "Aspring actress"
-  },
-  {
-    name: "Dennis",
-    job: "Bartender"
-  }
-];
+// const characters = [
+//   {
+//     name: "Charlie",
+//     job: "Janitor"
+//   },
+//   {
+//     name: "Mac",
+//     job: "Bouncer"
+//   },
+//   {
+//     name: "Dee",
+//     job: "Aspring actress"
+//   },
+//   {
+//     name: "Dennis",
+//     job: "Bartender"
+//   }
+// ];
 
 function MyApp() {
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+    } // the rest of the data
+  ]);
+
   return (
     <div className="container">
-      <Table characterData={characters} />
+      <Table 
+        characterData={characters} 
+        removeCharacter={removeOneCharacter}
+      />
+      
     </div>
   );
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
 }
 
 export default MyApp;
