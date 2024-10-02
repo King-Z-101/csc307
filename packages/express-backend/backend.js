209af3c2406f37f738b2334b720c174775a87c80
King-Z-101 <111461124+King-Z-101@users.noreply.github.com>
2024-10-02 11:47:59 -0700

    /users get regquest

---
+++ packages/express-backend/backend.js
@@ -0,0 +1,50 @@
+import express from "express";
+
+const users = {
+    users_list: [
+      {
+        id: "xyz789",
+        name: "Charlie",
+        job: "Janitor"
+      },
+      {
+        id: "abc123",
+        name: "Mac",
+        job: "Bouncer"
+      },
+      {
+        id: "ppp222",
+        name: "Mac",
+        job: "Professor"
+      },
+      {
+        id: "yat999",
+        name: "Dee",
+        job: "Aspring actress"
+      },
+      {
+        id: "zap555",
+        name: "Dennis",
+        job: "Bartender"
+      }
+    ]
+};
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+    res.send(users);
+  });
+
+app.listen(port, () => {
+  console.log(
+    `Example app listening at http://localhost:${port}`
+  );
+});
